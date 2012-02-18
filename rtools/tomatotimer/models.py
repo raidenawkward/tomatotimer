d9fc27d943d4031e169e18011c9a482bbf23947c
Richard Ma <richard.ma.19850509@gmail.com>
2012-02-19 00:09:20 +0800

    create task db

--- rtools/tomatotimer/models.py
+++ rtools/tomatotimer/models.py
@@ -2,7 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
+    PRIORITY_CHOICES = (
+        (u'ITP-CRITICAL',   u'ITP-CRITICAL'),
+        (u'ITP-RUSH',       u'ITP-RUSH'),
+        (u'ITP-NORMAL',     u'ITP-NORMAL'),
+        (u'ITP-LOW',        u'ITP-LOW'),
+    )
+    TASK_TYPE_CHOICES = (
+        ('TASK-TYPE-UNDEFINED', 'TASK-TYPE-UNDEFINED'),
+        ('TASK-TYPE-AI',        'TASK-TYPE-AI'),
+        ('TASK-TYPE-TODO',      'TASK-TYPE-TODO'),
+    )
+
     title = models.CharField(max_length=32)
     desc =  models.CharField(max_length=128)
-    priority =  models.CharField(max_length=16)
-    taskType =  models.CharField(max_length=16)
+    priority =  models.CharField(max_length=16, choices=PRIORITY_CHOICES)
+    taskType =  models.CharField(max_length=16, choices=TASK_TYPE_CHOICES)
